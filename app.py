598bdc9ed4af3076625afd76e5562fb2bfea0e62
ritesharyal666 <138800649+ritesharyal666@users.noreply.github.com>
2025-07-08 17:39:21 +0545

    Update app.py

--- app.py
+++ app.py
@@ -2,36 +2,30 @@ import streamlit as st
 import pickle
 import requests
 import gdown
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-TMDB_API_KEY_ = os.getenv('TMDB_API_KEY')
+TMDB_API_KEY_ = os.getenv('TMDB_API_KEY_')
 
+# Download the .pkl files from Google Drive
 
-def download_file_from_drive(file_id, output_path):
-    # Check if the file already exists, skip downloading if it does
-    if not os.path.exists(output_path):
-        # Use the correct format for gdown
-        gdown.download(f"https://drive.google.com/uc?id={file_id}", output_path, quiet=False)
-    else:
-        print(f"File {output_path} already exists. Skipping download.")
+def download_file_from_drive(file_url, output_path):
+    # Extract file ID from the Google Drive link
+    file_id = file_url.split('/d/')[1].split('/')[0]
+    
+    # Use the correct format for gdown
+    gdown.download(f"https://drive.google.com/uc?id={file_id}", output_path, quiet=False)
 
-# File IDs (directly provided)
-movies_file_id = "1d7mhUo4SzR45fSYi-tFQR741GYj9NmYw"
-similarity_file_id = "1a10EQd5ml0DW7iy85YYmKL1OXNZpW1yy"
+# File URLs from Google Drive
+movies_file_url = "https://drive.google.com/file/d/1d7mhUo4SzR45fSYi-tFQR741GYj9NmYw/view?usp=sharing"
+similarity_file_url = "https://drive.google.com/file/d/1a10EQd5ml0DW7iy85YYmKL1OXNZpW1yy/view?usp=sharing"
 
-# File paths for saving the .pkl files
-movies_file_path = "movies.pkl"
-similarity_file_path = "similarity.pkl"
-
-# Download the .pkl files if they don't exist
-download_file_from_drive(movies_file_id, movies_file_path)
-download_file_from_drive(similarity_file_id, similarity_file_path)
-
-
-
+# Example usage:
+download_file_from_drive(movies_file_url, "movies_file.csv")
+download_file_from_drive(similarity_file_url, "similarity_file.csv")
 
 # Load the DataFrame and similarity matrix
 choosen_df = pickle.load(open(movies_file_path, 'rb'))  # Load the DataFrame
